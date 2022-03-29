0a75d1cca4772ea7105d01ec475ab25022635098
MartinSanchezIut <martin.sanchez.iut@gmail.com>
2022-03-29 23:20:36 +0200

    Adding database dump (mongoexport)

---
+++ MongoExport/mongoexport.sh
@@ -0,0 +1,5 @@
+mongoexport --db NomDATABASE -c users --out dump/users.json --jsonArray
+mongoexport --db NomDATABASE -c ingredients --out dump/ingredients.json --jsonArray
+mongoexport --db NomDATABASE -c recettes --out dump/recettes.json --jsonArray 
+mongoexport --db NomDATABASE -c avis --out dump/avis.json --jsonArray
+mongoexport --db NomDATABASE -c likes --out dump/likes.json --jsonArray
